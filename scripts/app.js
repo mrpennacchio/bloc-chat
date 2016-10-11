7693929d143a7c73e716ef22b0414016f91e839d
Anthony <mrpennacchio@gmail.com>
2016-10-11 10:35:20 -0400

    checkpoint-2 on master branch

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCxT5b2pn0S_COdfI0uT_co2qo_yot-Wug",
+  authDomain: "bloc-chat-b2d90.firebaseapp.com",
+  databaseURL: "https://bloc-chat-b2d90.firebaseio.com",
+  storageBucket: "bloc-chat-b2d90.appspot.com",
+  messagingSenderId: "182689576764"
+};
+firebase.initializeApp(config);
+
+var app = angular.module("BlocChat", ["firebase"]);
+
+app.controller("RoomCtrl", function($scope, $firebaseArray) {
+  var ref = firebase.database().ref().child("rooms");
+  // create a synchronized array
+  $scope.rooms = $firebaseArray(ref);
+  // add new items to the array
+  // the room is automatically added to our Firebase database!
+  $scope.addRoom = function() {
+    $scope.rooms.$add({
+      text: $scope.newRoomText
+    });
+    $scope.newRoomText = '';
+  };
+
+  // $scope.addRoomMessage = function(roomId) {
+  //   var messagesRef = firebase.database().ref().child("rooms/" + roomId + "/messages");
+  //   $scope.roomMessages = $firebaseArray(messagesRef);
+  //
+  //   $scope.roomMessages.$add({
+  //     text: $scope.newRoomMessageText
+  //   });
+  //
+  //   $scope.newRoomMessageText = '';
+  // };
+  // click on `index.html` above to see $remove() and $save() in action
+});
