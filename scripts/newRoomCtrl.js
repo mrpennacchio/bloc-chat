ca46648e3edfaec3d01a26df5141f9e33fbf2c8a
Anthony <mrpennacchio@gmail.com>
2016-10-12 16:22:15 -0400

    checkpoint-4 with modal 4 merge with checkpoint 3

--- scripts/newRoomCtrl.js
+++ scripts/newRoomCtrl.js
@@ -4,6 +4,7 @@ app.controller("RoomCtrl", function($scope, $firebaseArray) {
   var ref = firebase.database().ref().child("rooms");
   // create a synchronized array
   $scope.rooms = $firebaseArray(ref);
+
   // add new items to the array
   // the room is automatically added to our Firebase database!
   $scope.addRoom = function() {
@@ -12,4 +13,23 @@ app.controller("RoomCtrl", function($scope, $firebaseArray) {
     });
     $scope.newRoomText = '';
   };
+
+//new room message
+  $scope.addRoomMessage = function(roomId) {
+    var messagesRef = firebase.database().ref().child("rooms/" + roomId);
+    $scope.roomMessages = $firebaseArray(messagesRef);
+
+    $scope.roomMessages.$add({
+      text: $scope.newRoomMessageText
+    });
+    $scope.newRoomMessageText = '';
+  };
+  // click on `index.html` above to see $remove() and $save() in action
+
+//getMessage
+  // $scope.getMessages = function(room) {
+  //   $scope.currentRoom = room;
+  //
+  // }
+
 });
